c115f89f2d05090c2f5188bc44f2ff46e4ba0540
grocko_t <grocko_t@Kilorbine.(none)>
2014-01-07 05:20:25 +0100

    mdif, game fonctionnel, masi pasfinissable

--- Include/PuzzleGame.hh
+++ Include/PuzzleGame.hh
@@ -9,15 +9,24 @@
 #include <list>
 #include "PuzzleSprite.hh"
 
+bool    compare_id(const PuzzleSprite*, const PuzzleSprite*);
+
 class PuzzleGame
 {
   std::string	_picture;
   sf::RenderWindow	&_window;
   bool	_isTarget;
+  bool	_diff;
 
 public:
   PuzzleGame(sf::RenderWindow&);
   ~PuzzleGame();
+  bool	gestEvent(sf::Event&, std::list<PuzzleSprite*>&);
+  void	upUp( std::list<PuzzleSprite*>&);
+  void	upRight( std::list<PuzzleSprite*>&);
+  void	upLeft( std::list<PuzzleSprite*>&);
+  void	upDown( std::list<PuzzleSprite*>&);
+  void	colorRect(sf::Color, int, int, int, int);
   void	mainGame(sf::Texture&, std::string);
   void	display(std::list<PuzzleSprite*>&, int);
   void	randImg(std::list<PuzzleSprite*>&);
